52080c213a031be14866b6c23d122933ed8d6535
Ravi Singh Raghuvanshi <ravisinghraghuvanshi584@gmail.com>
2026-07-14 11:30:28 +0530

    Update init.databse.sql
    Clarified the purpose of the script in the comments.

--- scripts/init.databse.sql
+++ scripts/init.databse.sql
@@ -3,7 +3,7 @@
 Create Database and Schemas
 =============================================================
 Script Purpose:
-    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+    This purpose of this script is to create a new database named 'DataWarehouse' after checking if it already exists. 
     If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
     within the database: 'bronze', 'silver', and 'gold'.
 	
